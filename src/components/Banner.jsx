ef39b431a1b17d8763c22a4ee287d2019e8f4afe
Marie Sheela JOSEPH LOUIS SELVARAJ <sheelajoseph200@gmail.com>
2023-05-20 22:32:17 +0200

    headline changed

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -9,7 +9,7 @@ function Banner() {
           <div className='col-l-6'>
             <p>Hello! My name  is </p>
             <h1>Ms. Marie Sheela JOSEPH LOUIS SELVARAJ</h1>
-            <p>I am a Software Engineer living in France. I create Front end applications with React mostly and I am curious to learn new technologies.</p>
+            <p>I am a Software Engineer living in France. I build responsive website front end using ReactJS and I am curious to learn new technologies.</p>
           </div>
           <div className='col-l-6'>
             <img src='./assets/sheela_graduation.png' alt='sheela'/>
